bd28c5272b7b8f2406489a8eba2cd6cacda78a31
docBliny <docBliny@users.noreply.github.com>
2020-04-27 21:55:16 -0500

    Clean up and knock off Aiden Wallis's front-end.

--- README.md
+++ README.md
@@ -3,11 +3,14 @@ Simply Roon extension to display what's playing in each zone. Intended to be use
 
 Note that I don't really intend to make this more configurable at this point due to time restrictions...
 
+## Credits
+Credits to Aiden Wallis - https://github.com/aidenwallis/nowplaying . I pretty much ended up grabbing the HTML, CSS, and Javascript directly from that.
+
 ## Requirements
 Node 14+ required.
 
 ## Using Accessing the display
-* Run `npm ci` or `nom install`
+* Run `npm ci` or `npm install`
 * Start the webserver with `npm start` or `node .`
 * Open Roon, and go to Settings / Extensions, and then click Enable.
 * Open your browser to http://YOUR-IP:8686 .
@@ -15,8 +18,13 @@ Node 14+ required.
 ### Configuring OBS
 * Add a new Browser Source
 * Set the URL to wherever you're running the extension web server
-* Set the width to 1300 pixels and height to 300 pixels
-* Adjust the placement on your screen
+* Set height to 300 pixels
+* Set the width to your desired width
+* Set the FPS to 60
+* Adjust the placement and size on your screen
+
+## Troubleshooting
+* If the Roon zone isn't available, you'll get the "404 - Not found" page. However, it has a margin at the top, so that it doesn't accidentally show up in your stream, but you can open the link in your browser and see the error.
 
 ## TODO
 * Dockerize
--- public/js/now-playing.js
+++ public/js/now-playing.js
@@ -1,25 +1,174 @@
 console.log("Connecting to " + config.zoneName);
-var socket = io("/zones/" + config.zoneName);
-socket.on("zoneStatus", function(msg) {
+
+var App = {};
+
+var container = document.getElementById("container");
+var currentAlbumCover = document.getElementById("album-current");
+var newAlbumCover = document.getElementById("album-new");
+var artistsElement = document.getElementById("artists");
+var songName = document.getElementById("name");
+
+App.currentSong = "";
+App.currentCover = "";
+App.open = false;
+App.fullyOpen = false;
+App.firstAlbumLoad = true;
+App.scrollingSong = false;
+App.scrollingArtists = false;
+
+App.onZoneUpdate = function(msg) {
   if(msg && msg.now_playing && msg.now_playing.two_line) {
-    var songName = msg.now_playing.two_line.line1;
-    var artistName = msg.now_playing.two_line.line2;
-    var imageUrl = "/api/images/" + msg.now_playing.image_key;
+    if(msg.state != "playing" && msg.state != "loading") {
+      if(App.open) {
+        App.close();
+      }
+    } else {
+      const data = {
+        songName: msg.now_playing.two_line.line1,
+        artists: msg.now_playing.two_line.line2,
+        albumCover: "/api/images/" + msg.now_playing.image_key,
+      };
 
-    var coverArtEl = document.getElementById("cover-art");
-    if(!coverArtEl.src.endsWith(imageUrl)) {
-      console.log(coverArtEl.src, imageUrl)
-      coverArtEl.src = imageUrl;
+      if(App.open) {
+        App.startUpdate(data);
+      } else {
+        App.openElement();
+        setTimeout(App.startUpdate(data), 1200);
+      }
     }
-
-    var artistNameEl = document.getElementById("artist-name");
-    if(artistNameEl.textContent !== artistName) {
-      artistNameEl.textContent = artistName;
-    }
-
-    var songNameEl = document.getElementById("song-name");
-    if(songNameEl.textContent !== songName) {
-      songNameEl.textContent = songName;
+  } else {
+    // Nothing playing
+    if(App.open) {
+      App.close();
     }
   }
-});
+}
+
+App.close = function() {
+  App.open = false;
+  App.fullyOpen = false;
+  App.firstAlbumLoad = true;
+  App.currentCover = "";
+  App.currentSong = "";
+  songName.classList.add("drop");
+  setTimeout(function() {
+    artistsElement.classList.add("drop");
+  }, 350);
+  setTimeout(function() {
+    songName.innerHTML = "";
+    artistsElement.innerHTML = "";
+    songName.className = "";
+    artistsElement.className = "";
+    App.scrollingSong = false;
+    container.classList.remove("active");
+  }, 800);
+  setTimeout(function() {
+    container.classList.remove("raise");
+  }, 1350);
+  setTimeout(function() {
+    currentAlbumCover.classList.remove("active");
+    currentAlbumCover.src = "";
+    newAlbumCover.classList.remove("active");
+    newAlbumCover.src = "";
+  }, 1800);
+};
+
+App.startUpdate = function(data) {
+  if(App.currentSong !== data.songName) {
+    App.currentSong = data.songName;
+    App.updateSongName(data.artists, data.songName);
+  }
+  if(App.currentCover !== data.albumCover) {
+    App.currentCover = data.albumCover;
+    App.updateCover(data.albumCover);
+  }
+};
+
+App.openElement = function() {
+  App.open = true;
+  container.classList.add("raise");
+  setTimeout(function() {
+    container.classList.add("active");
+  }, 550);
+  setTimeout(function() {
+    App.fullyOpen = true;
+  }, 1500);
+}
+
+App.updateSongName = function(artists, name) {
+  artistsElement.classList.remove("active");
+  artistsElement.classList.remove("scrolling");
+
+  setTimeout(function() {
+    songName.classList.remove("active");
+    songName.classList.remove("scrolling");
+  }, 200);
+
+  setTimeout(function() {
+    artistsElement.textContent = artists;
+    artistsElement.classList.add("active");
+  }, 550);
+
+  setTimeout(function() {
+    const maxWidth = container.offsetWidth - (300 + 30);
+    void artistsElement.offsetWidth;
+
+    if(artistsElement.offsetWidth > maxWidth) {
+      if(!App.scrollingArtists) {
+        App.scrollingArtists = true;
+        artistsElement.classList.add("scrolling");
+      }
+    } else {
+      if(App.scrollingArtists) {
+        App.scrollingArtists = false;
+        artistsElement.classList.remove("scrolling");
+      }
+    }
+  }, 2000);
+
+  setTimeout(function() {
+    songName.textContent = name;
+    songName.classList.add("active");
+  }, 750);
+
+  setTimeout(function() {
+    const maxWidth = container.offsetWidth - (300 + 30);
+    void songName.offsetWidth;
+
+    if(songName.offsetWidth > maxWidth) {
+      if(!App.scrollingSong) {
+        App.scrollingSong = true;
+        songName.classList.add("scrolling");
+      }
+    } else {
+      if(App.scrollingSong) {
+        App.scrollingSong = false;
+        songName.classList.remove("scrolling");
+      }
+    }
+  }, 2000);
+};
+
+App.updateCover = function(cover) {
+  newAlbumCover.src = cover;
+  newAlbumCover.onload = function() {
+    newAlbumCover.className += " active";
+    if(App.firstAlbumLoad) {
+      currentAlbumCover.classList.add("active");
+    }
+    setTimeout(function() {
+      currentAlbumCover.src = cover;
+      newAlbumCover.classList.remove("active");
+      setTimeout(function() {
+        newAlbumCover.src = "";
+      }, 500);
+    }, 450);
+  };
+};
+
+App.start = function() {
+  var socket = io("/zones/" + config.zoneName);
+  socket.on("zoneStatus", App.onZoneUpdate);
+};
+
+App.start();
